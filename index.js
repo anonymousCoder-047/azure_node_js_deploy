bdbe7e31c796e3e6d3d1cd1543467ce1a121f300
anonymousCoder047 <anonymouscoder047@gmail.com>
2024-09-01 00:59:51 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const { NODE_ENV } = require('./config/config');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const http = require('http');
+
+
+// initializing express class
+const app = express();
+
+// middlewares
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors('*'))
+
+
+if(NODE_ENV !== 'prod') {
+    morgan('tiny')
+}
+
+app.get('/', (req, res) => {
+    res.json({ "status": "200", "content": "Hello World" });
+})
+
+app.get('/test', (req, res) => {
+    res.json({ "status": "200", "content": "testing REST API" });
+})
+
+// explicitly creating a HTTP server from express object
+const server = http.createServer(app);
+
+module.exports = server;
